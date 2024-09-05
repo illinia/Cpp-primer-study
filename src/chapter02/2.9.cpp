3f664465aca9a956cbefd7d9b41b91e22fdb66d1
TAEMIN <seolin1218@gmail.com>
2024-09-05 18:03:13 +0900

    초기화

---
+++ src/chapter02/2.9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	//std::cin >> int input_value;
+	//int i = { 3.14 };
+	//std::cout << i << std::endl;
+
+	//double salary = wage = 9999.99;
+
+	int i = 3.14;
+	std::cout << i << std::endl;
+}
