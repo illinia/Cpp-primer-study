9b91d24806dcdb5998530e84b2c18060fd3f7fbf
TAEMIN <seolin1218@gmail.com>
2024-09-08 20:45:27 +0900

    포인터

---
+++ src/chapter02/2.20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int i = 42;
+	int* pl = &i;
+	*pl = *pl * *pl;
+
+	std::cout << *pl << std::endl;
+
+	int i = 0;
+	//double* dp = &i;
+	//int *ip = i;
+
+	int* p = &i;
+
+	if (p);
+	if (*p);
+
+	return 0;
+}
