e3271753cafcc01fcff74d42eb4c4a310938db56
TAEMIN <seolin1218@gmail.com>
2024-09-18 16:24:02 +0900

    3.3.2 벡터 요소 추가

---
+++ src/chapter03/3.3.2.cpp
@@ -0,0 +1,14 @@
+#include "include_header.h"
+
+int main()
+{
+	/*vector<int> v2;
+	for (int i = 0; i != 100; ++i)
+		v2.push_back(i);*/
+
+	string word;
+	vector<string> text;
+	while (cin >> word) {
+		text.push_back(word);
+	}
+}
