eed24e440b286e81b06547a20499bc0e9034c421
TAEMIN <seolin1218@gmail.com>
2024-09-21 18:05:22 +0900

    3.5.1 내장 타입 정의와 초기화하기

---
+++ src/chapter03/3.5.cpp
@@ -0,0 +1,34 @@
+#include "include_header.h"
+
+int main()
+{
+	unsigned cnt = 42;
+	constexpr unsigned sz = 42;
+
+	int arr[10];
+	int* parr[sz];
+	//string bad[cnt];
+	//string strs[get_size()];
+
+	const unsigned sz = 3;
+	int ia1[sz] = { 1,2,3 };
+	int a2[] = { 0,1,2 };
+	int a3[5] = { 0,1,2 };
+	string a4[3] = { "hi", "bye" };
+	//int a5[2] = { 0, 1, 2 };
+
+	char a1[] = { 'C', '+', '+' };
+	char a2[] = { 'C', '+', '+', '\0' };
+	char a3[] = "C++";
+	//const char a4[6] = "Daniel";
+	int a[] = { 0, 1, 2 };
+	//int a2[] = a;
+	//a2 = a;
+
+	int* ptrs[10];
+	//int& refs[10];
+	int(*Parray)[10] = &arr;
+	int(&arrRef)[10] = arr;
+
+	int* (&array)[10] = ptrs;
+}
---
+++ src/chapter03/practice/3.28.cpp
@@ -0,0 +1,10 @@
+#include "include_header.h"
+
+string sa[10];
+int ia[10];
+
+int main()
+{
+	string sa2[10];
+	int ia2[10];
+}
