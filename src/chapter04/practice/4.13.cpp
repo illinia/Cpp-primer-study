8ac684eedba4a6a5d5f134768f4cf0e1f848de89
TAEMIN <seolin1218@gmail.com>
2024-09-30 22:45:07 +0900

    4.4 대입 연산자

---
+++ src/chapter04/practice/4.13.cpp
@@ -0,0 +1,16 @@
+#include "include_header.h"
+
+int main() {
+	int i; double d;
+	d = i = 3.5;
+	cout << d << " " << i << endl;
+
+	i = d = 3.5;
+	cout << d << " " << i << endl;
+
+	//if (42 = i) {}
+	//if (i = 42) {}
+
+	double dval; int ival; int* pi;
+	//dval = ival = pi = 0;
+}
