f6dea2f632a93830f087df98811b240346bb4440
TAEMIN <seolin1218@gmail.com>
2024-10-03 14:23:37 +0900

    4.10 쉼표 연산자

---
+++ src/chapter04/4.10.cpp
@@ -0,0 +1,14 @@
+#include "include_header.h"
+
+int main() {
+	vector<int> ivec{ 0,1,2,3,4,5,6,7,8,9 };
+	vector<int>::size_type cnt = ivec.size();
+
+	for (vector<int>::size_type ix = 0; ix != ivec.size(); ++ix, --cnt) {
+		ivec[ix] = cnt;
+	}
+
+	for (auto i : ivec) {
+		cout << i << " ";
+	}
+}
---
+++ src/chapter04/practice/4.32.cpp
@@ -0,0 +1,7 @@
+#include "include_header.h"
+
+int main() {
+	constexpr int size = 5;
+	int ia[size] = { 1,2,3,4,5 };
+	for (int* ptr = ia, ix = 0; ix != size && ptr != ia+size; ++ix, ++ptr) {}
+}
