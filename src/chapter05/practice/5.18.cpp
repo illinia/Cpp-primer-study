1f32d978354ee43380f950298604f10964c3211b
TAEMIN <seolin1218@gmail.com>
2024-10-14 20:36:08 +0900

    5.4.4 do while 문

---
+++ src/chapter05/practice/5.18.cpp
@@ -0,0 +1,16 @@
+#include "include_header.h"
+
+int main() {
+	//do
+	//	int v1, v2;
+	//	cout << "Please enter two numbers to sum:";
+	//	if (cin >> v1 >> v2)
+	//		cout << "Sum is: " << v1 + v2 << endl;
+	//while (cin);
+
+	//do {} while (int ival = rand());
+
+	//do {
+	//	int ival = rand();
+	//} while (ival);
+}
