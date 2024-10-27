7c46cd18d40ad686d55b4c41941782a4ae27661b
TAEMIN <seolin1218@gmail.com>
2024-10-27 18:02:45 +0900

    6.2.2 참조자로 인자 전달

---
+++ src/chapter06/practice/6.12.cpp
@@ -0,0 +1,17 @@
+#include "include_header.h"
+
+void exchangeInt(int& p1, int& p2) {
+	int tmp;
+	tmp = p1;
+	p1 = p2;
+	p2 = tmp;
+}
+
+int main() {
+	int i1 = 1, i2 = 40;
+	exchangeInt(i1, i2);
+
+	cout << "i1:" << i1 << " i2:" << i2;
+
+	return 0;
+}
