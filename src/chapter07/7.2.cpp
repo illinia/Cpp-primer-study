c471043704a9f4feceef5ed98aa8df8cc8c80d16
TAEMIN <seolin1218@gmail.com>
2024-11-11 23:43:06 +0900

    7.2.1 프렌드

---
+++ src/chapter07/7.2.cpp
@@ -0,0 +1,45 @@
+#include "string"
+#include <iostream>
+
+using namespace std;
+
+class Sales_data {
+	friend Sales_data add(const Sales_data&, const Sales_data&);
+	friend istream& read(istream&, Sales_data&);
+	friend ostream& print(ostream&, const Sales_data&);
+public:
+	Sales_data() = default;
+	Sales_data(const string& s, unsigned n, double p) :
+		bookNo(s), units_sold(n), revenue(p* n) {}
+	Sales_data(istream&);
+	string isbn() const { return bookNo; }
+	Sales_data& combine(const Sales_data&);
+
+private:
+	double avg_price() const
+	{
+		return units_sold ? revenue / units_sold : 0;
+	}
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs)
+{
+	Sales_data sum = lhs;
+	sum.combine(rhs);
+	return sum;
+}
+istream& read(istream& is, Sales_data& item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
+ostream& print(ostream& os, const Sales_data& item)
+{
+	os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+	return os;
+}
--- src/chapter07/practice/7.15.h
+++ src/chapter07/practice/7.15.h
@@ -3,13 +3,29 @@
 using namespace std;
 
 struct Person {
+	friend istream& read(istream&, Person&);
+	friend ostream& print(ostream&, const Person&);
+public:
 	Person() = default;
 	Person(const string& n, const string& a) :
 		name(n), address(a) {}
 
-	string name;
-	string address;
-
 	const string get_name() const { return name; }
 	const string get_address() const { return address; }
+
+private:
+	string name;
+	string address;
 };
+
+istream& read(istream& is, Person& person)
+{
+	is >> person.name >> person.address;
+	return is;
+}
+
+ostream& print(ostream& os, const Person& person)
+{
+	os << person.name << " " << person.address;
+	return os;
+}
